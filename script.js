0d2e2e9427926164cddc6de777ec71813c3bdf91
TomU <turquhart6@hotmail.com>
2018-10-31 15:39:26 -0600

    updated from laptop

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-var parent = document.getElementById("list1");
-var child = document.getElementById("item1");
-var button = document.getElementById("adios");
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.getElementById("list1");
 
-function removeItem(){
-	parent.removeChild(child);
+function addItemOnClick(){
+	if(input.value.length > 0){
+		addItem();
+	}
 }
 
-button.addEventListener("click", removeItem);
+function addItemOnEnter(event){
+	if(input.value.length > 0 && event.keyCode==13){
+		addItem();
+	}
+}
+
+function addItem(){
+	li = document.createElement("li");
+	li.classList.add("listItem");
+	li.appendChild(document.createTextNode(input.value + "  "));
+	erase = document.createElement("button");
+	erase.classList.add("deleteBtn");
+	erase.appendChild(document.createTextNode("X"));
+	erase.addEventListener("click", deleteItem);
+	li.appendChild(erase);
+	ul.appendChild(li);
+	input.value = "";
+}
+
+function deleteItem(){
+	this.parentNode.remove();
+}
+
+button.addEventListener("click", addItemOnClick);
+input.addEventListener("keypress", addItemOnEnter);
